7df1d2dbdf02d1634621ce727466d508fd5a4364
bakarr86 <bakarr19861@gmail.com>
2024-09-06 06:44:31 -0400

    app . dependancies import

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
-
-const server = http.createServer(function(reg, res) {
-res.writeHead(200, { 'Content-Type' : 'text/html' })
-fs.readFile('index.html', function(error, data) {
-if (error) {
-    res.writeHead(404)
-    res.write('Error: File Not Found')
-    } else {
-    res.write(data)
-    }
-res.end()
-})
-})
-
-//server.listen(port, function(error) {
-  //  if (error) {
-    //    console.log('Something went wrong', error)
-    //} else {
-      //  console.log('Server is listening on port' + port)
-    //}
-//})
+const express = require('express');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./scratch');
+const app = express();
+const port = 3000;
