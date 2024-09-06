a57691b2292700776c9f66cd7d26bf15856fc2bd
bakarr86 <bakarr19861@gmail.com>
2024-09-06 06:48:59 -0400

    view engine setup

--- app.js
+++ app.js
@@ -13,3 +13,14 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Custom middleware: Logger
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
